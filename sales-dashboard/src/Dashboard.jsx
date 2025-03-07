f3879ec750f4e0d2fc5b094aa63fa0a1d7a1fe14
Murtaza Najmi <murtazanajmi65@gmail.com>
2025-03-07 11:12:49 +0500

    commit init

--- sales-dashboard/src/Dashboard.jsx
+++ sales-dashboard/src/Dashboard.jsx
@@ -37,12 +37,12 @@ const Dashboard = () => {
     [
       { name: "Jane Smith", total: 15, customers: 12, coverage: "90%" },
       { name: "Bob Martin", total: 10, customers: 7, coverage: "70%" },
-      { name: "Ali", total: 10, customers: 15, coverage: "95%" },
+      { name: "Ahmed", total: 8, customers: 10, coverage: "75%" },
     ],
     [
       { name: "Mark Lee", total: 10, customers: 7, coverage: "70%" },
       { name: "Sarah Wilson", total: 16, customers: 13, coverage: "95%" },
-      { name: "Ali", total: 10, customers: 15, coverage: "95%" },
+      { name: "Bilal", total: 7, customers: 14, coverage: "85%" },
     ],
     [
       { name: "Anna Taylor", total: 18, customers: 15, coverage: "85%" },
@@ -94,35 +94,35 @@ const Dashboard = () => {
 
      
       {isSidebarOpen && (
-        <div className="absolute top-0 left-0 w-48 h-screen bg-gradient-to-r
+        <div className="absolute top-0 left-0 w-50 h-screen bg-gradient-to-r
                 from-blue-800 to-purple-950 text-white shadow-lg z-50">
           <button onClick={toggleSidebar}>
-            <img className='mx-5 py-4 h-20' src='https://flowbite.com/docs/images/logo.svg' alt='Dashboard Logo'/>
-            <span className="mx-4" >GSM-SALES</span>
+            <img className='mx-4 py-4 h-18' src='https://flowbite.com/docs/images/logo.svg' alt='Dashboard Logo'/>
+            <span className="mx-4 font-serif" >GSM-SALES</span>
             
             </button>
            
           <ul className="p-3 space-y-8">
           <li>
-              <a href="#" className="block hover:text-blue-400 font-serif border-2">Dashboard</a>
+              <a href="#" className="block hover:text-blue-400 font-serif border-b-2 m-1">Dashboard</a>
             </li>
             <li>
-              <a href="#" className="block hover:text-blue-400 font-serif border-2">Customers</a>
+              <a href="#" className="block hover:text-blue-400 font-serif border-b-2 m-1">Customers</a>
             </li>
             <li>
-              <a href="#" className="block hover:text-blue-400 font-serif border-2">Sales</a>
+              <a href="#" className="block hover:text-blue-400 font-serif border-b-2 m-1">Sales</a>
             </li>
             <li>
-              <a href="#" className="block hover:text-blue-400 font-serif border-2">Reports</a>
+              <a href="#" className="block hover:text-blue-400 font-serif border-b-2 m-1">Reports</a>
             </li>
             <li>
-              <a href="#" className="block hover:text-blue-400 font-serif border-2">Targets</a>
+              <a href="#" className="block hover:text-blue-400 font-serif border-b-2 m-1">Targets</a>
             </li>
           </ul>
         </div>
       )}
     </div>
-       <div className='text-white font-bold text-2xl m-2  '>Dashboard
+       <div className='text-white font-bold text-2xl m-2'>Dashboard
        </div>
        </div>
        <ul className='flex absolute top-0 right-145 m-2'>
@@ -178,6 +178,7 @@ const Dashboard = () => {
   <path fill-rule="evenodd" d="M12 20a7.966 7.966 0 0 1-5.002-1.756l.002.001v-.683c0-1.794 1.492-3.25 3.333-3.25h3.334c1.84 0 3.333 1.456 3.333 3.25v.683A7.966 7.966 0 0 1 12 20ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10c0 5.5-4.44 9.963-9.932 10h-.138C6.438 21.962 2 17.5 2 12Zm10-5c-1.84 0-3.333 1.455-3.333 3.25S10.159 13.5 12 13.5c1.84 0 3.333-1.455 3.333-3.25S13.841 7 12 7Z" clip-rule="evenodd"/>
 </svg>
 
+
         </button>
 
         {showDropdown && (
@@ -229,45 +230,38 @@ const Dashboard = () => {
 
 
 
-      <div class="relative h-30 bg-gradient-to-r
-                from-blue-600 to-purple-800 mt-20">
+      <div class="relative h-30 bg-gradient-to-r  from-blue-600 to-purple-800 mt-20">
         
-        <div class="absolute inset-0 
-                    bg-gray-800 opacity-50 
-                    z-10">
+        <div class="absolute inset-0 bg-gradient-to-r  from-blue-700 to-purple-800 opacity-50 z-10">
         </div>
-
-        <div class="absolute inset-0  flex
-                    items-center justify-between
-                    text-white z-20  m-4">
-            <div class="bg-gray-500 p-2 
-                        rounded-lg shadow-lg w-30">
-                            <svg class="w-6 h-6 text-gray-800 dark:text-white float-right" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+        <div class="absolute inset-0  flex items-center justify-between  dark:text-blue-600 z-20  m-4">
+            <div class="bg-white p-2  rounded-lg shadow-lg w-45 h-25">
+   <svg class="w-12 h-15 mr-1 text-gray-800 dark:text-blue-600 float-right" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4.5V19a1 1 0 0 0 1 1h15M7 14l4-4 4 4 5-5m0 0h-3.207M20 9v3.207"/>
 </svg>
-                <h1 class="text-md font-bold mb-1 ">
+                <h1 class="text-lg font-bold mt-1">
                     Item Sales
                 </h1>
-                <p class="text-sm text-center">
+                <p class="text-sm ml-2 mt-4  float-right ">
                     YTD TP Value
                 </p>
             </div>
-            <div class="bg-gray-500 p-2 
-                        rounded-lg shadow-lg w-30">
-    <svg class="w-6 h-6 text-gray-800 dark:text-white float-right" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <div class="bg-white p-2   rounded-lg shadow-lg w-50 h-25 ">
+    <svg class="w-12 h-15 mr-1 text-gray-800 dark:text-blue-600 float-right" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4.5V19a1 1 0 0 0 1 1h15M7 14l4-4 4 4 5-5m0 0h-3.207M20 9v3.207"/>
 </svg>
 
-                <h1 class="text-md font-bold mb-1">
+                <h1 class="text-lg font-bold mt-2">
                    New Sales
                 </h1>
-                <p class="text-sm text-center">
+                <h2 className='float-left text-md text-purple-500'>56520,352</h2>
+                <p class="text-sm float-right ml-2 mt-1">
                     YTD TP Value
                 </p>
             </div>
-            <div class="bg-gray-500 p-2 
+            <div class="bg-white p-2 
                         rounded-lg shadow-lg w-30">
-                          <svg className="w-6 h-6 text-gray-800 dark:text-white float-right" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                          <svg className="w-6 h-6 text-gray-800 dark:text-blue-600 float-right" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312"/>
 </svg>
 
@@ -278,12 +272,16 @@ const Dashboard = () => {
                     YTD TP Value
                 </p>
             </div>
-            <div class="bg-gray-500 p-2 
-                        rounded-lg shadow-lg w-30">
+            <div class="bg-white p-2 rounded-lg shadow-lg w-30">
+                          <svg className="w-6 h-6 text-gray-800 dark:text-blue-600 float-right " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+  <path d="M6 2c-1.10457 0-2 .89543-2 2v4c0 .55228.44772 1 1 1s1-.44772 1-1V4h12v7h-2c-.5523 0-1 .4477-1 1v2h-1c-.5523 0-1 .4477-1 1s.4477 1 1 1h5c.5523 0 1-.4477 1-1V3.85714C20 2.98529 19.3667 2 18.268 2H6Z"/>
+  <path d="M6 11.5C6 9.567 7.567 8 9.5 8S13 9.567 13 11.5 11.433 15 9.5 15 6 13.433 6 11.5ZM4 20c0-2.2091 1.79086-4 4-4h3c2.2091 0 4 1.7909 4 4 0 1.1046-.8954 2-2 2H6c-1.10457 0-2-.8954-2-2Z"/>
+</svg>
+
                 <h1 class="text-md font-bold mb-1">
                     New Visitor
                 </h1>
-                <p class="text-sm text-center">
+                <p class="text-sm ">
                     YTD TP Value
                 </p>
             </div>
@@ -339,7 +337,7 @@ const Dashboard = () => {
             datasets:[{
 
               label: 'Revenue',
-              data: [200,600,500,700,600,800,700,750,1200],
+              data: [200,600,500,700,600,800,700,750,200],
 
                 backgroundColor: [
                   "rgba(43, 63, 229, 0.8 )"
@@ -350,7 +348,7 @@ const Dashboard = () => {
             {
 
               label: 'Loss',
-              data: [90,80,70,60,50,40,30,20,1700],
+              data: [190,180,100,160,500,430,430,200,700],
 
                backgroundColor: [
                   "rgba(54, 162, 235, 0.5 )"
